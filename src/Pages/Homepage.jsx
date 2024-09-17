da1d89d254ab89f6716541cd4587d833a9636145
Fattan2005 <fattanhibrizi@gmail.com>
2024-09-17 13:17:39 +0700

    business sector, fixed mission resp, navbar fixed when scroll

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -2,9 +2,18 @@ import { Container, Row, Col } from "react-bootstrap"
 
 import HeroImage from "../assets/tju-logo1.png"
 
+import frame1 from "../assets/bussec/Frame 1.png"
+import frame2 from "../assets/bussec/Frame 2.png"
+import frame3 from "../assets/bussec/Frame 3.png"
+import frame4 from "../assets/bussec/Frame 4.png"
+import frame5 from "../assets/bussec/Frame 5.png"
+import frame6 from "../assets/bussec/Frame 6.png"
+
 const Homepage = () => {
   return (
     <div className="homepage">
+
+      {/* header */}
       <header className="w-100 overflow-hidden pb-0">
         <Container>
           <Row className="row-cols-lg-2 row-cols-1 px-2">
@@ -19,40 +28,85 @@ const Homepage = () => {
           </Row>
         </Container>
       </header>
+
+      {/* about us */}
       <div className="w-100 min-vh-100" id="Aboutus">
         <Container>
           <Row>
-            <h1 className="title mb-5 text-uppercase">About Us</h1>
-            <div className="text-white fw-bold mt-5">
-              <p>Place, Date established &nbsp; : Jakarta, June 03, 2022 </p>
-              <p>Type of business &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : Engineering & Construction </p>
-              <p>Commissioner &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : JON BAHRUM MUHID (Mr.) </p>
-              <p>President Director &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : RAHMAD HANAFI (Mr.) </p>
-              <p>Market Established &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : National </p>
-              <p>Payment Terms &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : Down Payment & Progress Payment </p>
-              <p>Business License &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : 03062200586790005</p>
-            </div>
+            <Col>
+              <h1 className="title mb-5 text-uppercase">About Us</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="text-white fw-bold">
+                <p>Place, Date established &nbsp; : Jakarta, June 03, 2022 </p>
+                <p>Type of business &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : Engineering & Construction </p>
+                <p>Commissioner &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : JON BAHRUM MUHID (Mr.) </p>
+                <p>President Director &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : RAHMAD HANAFI (Mr.) </p>
+                <p>Market Established &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : National </p>
+                <p>Payment Terms &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : Down Payment & Progress Payment </p>
+                <p>Business License &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : 03062200586790005</p>
+              </div>
+            </Col>
           </Row>
         </Container>
       </div>
+
+      {/* mission vision */}
       <div className="w-100 min-vh-100" id="Mission">
         <Container>
           <Row>
-            <h1 className="title mb-2 text-uppercase">Mission</h1>
-            <ul className="text-white fw-bold">
-              <li>Maintaining intensive communication along with valuable business partners to increase business acceleration and create a long term mutual business relationship.</li>
-              <li>Conducting a continuous program of Research, Design And Development program to create a sustainable business concept.</li>
-              <li>Continuously developing original design products.</li>
-              <li>Exploring and maximizing the use of new knowledge, design and construction method.</li>
-              <li>Managing the human resources professionally.</li>
-            </ul>
+            <Col>
+              <h1 className="title mb-2 text-uppercase">Mission</h1>
+              <ul className="text-white fw-bold">
+                <li>Maintaining intensive communication along with valuable business partners to increase business acceleration and create a long term mutual business relationship.</li>
+                <li>Conducting a continuous program of Research, Design And Development program to create a sustainable business concept.</li>
+                <li>Continuously developing original design products.</li>
+                <li>Exploring and maximizing the use of new knowledge, design and construction method.</li>
+                <li>Managing the human resources professionally.</li>
+              </ul>
+            </Col>
           </Row>
           <Row>
-            <h1 className="title mb-2 text-uppercase">Vision</h1>
-            <ul className="text-white fw-bold">
-              <li>Being the leading role in Engineering, Procurement And Construction Company which combining Indonesian selected workmanship, clients, legals and with a continuous Research of Construction method, Design & Development program.</li>
-              <li>Bringing the excellence of Indonesian workmanship to win the global competition in EPC industry.</li>
-            </ul>
+            <Col>
+              <h1 className="title mb-2 text-uppercase">Vision</h1>
+              <ul className="text-white fw-bold">
+                <li>Being the leading role in Engineering, Procurement And Construction Company which combining Indonesian selected workmanship, clients, legals and with a continuous Research of Construction method, Design & Development program.</li>
+                <li>Bringing the excellence of Indonesian workmanship to win the global competition in EPC industry.</li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+
+      {/* business sector */}
+      <div className="w-100 min-vh-100" id="Bussec">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="title mb-5 text-uppercase">Business Sector</h1>
+            </Col>
+          </Row>
+          <Row className="row-cols-lg-3 row-cols-sm-2 row-cols-1 g-5 photo mx-auto">
+            <Col>
+              <img src={frame1} alt="Operation & Maintenance Service" />
+            </Col>
+            <Col>
+              <img src={frame2} alt="Civil Construction" />
+            </Col>
+            <Col>
+              <img src={frame3} alt="Electrical & Instrumental Construction/Instalation" />
+            </Col>
+            <Col>
+              <img src={frame4} alt="Mechanical Construction/Instalation" />
+            </Col>
+            <Col>
+              <img src={frame5} alt="Engineering Design" />
+            </Col>
+            <Col>
+              <img src={frame6} alt="Project Management Service" />
+            </Col>
           </Row>
         </Container>
       </div>
--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,21 +1,37 @@
 import { Navbar, Container, Nav, Row, Col } from "react-bootstrap"
+import { useState, useEffect } from 'react';
 import Icon from "../assets/TJU.png"
 import { NavLink } from 'react-router-dom';
 
 const NavbarComponent = () => {
+  const [changeColor, setChangeColor] = useState(false);
+
+  const changeBackgroundColor = () => {
+    if(window.scrollY > 10){
+      setChangeColor(true);
+    }else{
+      setChangeColor(false);
+    }
+  };
+
+  useEffect(() => {
+    changeBackgroundColor();
+
+    window.addEventListener('scroll', changeBackgroundColor)
+  });
   return (
       <div className="background">
-        <Navbar expand="lg" className="bg-body-tertiary align-items-center">
+        <Navbar expand="lg" className={changeColor ? "color-active" : ""}>
           <Container>
-            <Navbar.Brand href="/" className=' d-flex gap-3 align-items-center'>
+            <Navbar.Brand href="/" className=' d-flex gap-2 gap-md-3 align-items-center'>
               <img src={Icon} alt="Icon Tangkal Jati Utama" />
               <p className="fw-bold">Tangkal Jati Utama</p>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-              <Nav className="px-4 gap-lg-5 nav-link text-white rounded-3 fw-bold">
+              <Nav className="px-4 gap-lg-5 nav-link text-white rounded-3 fw-bold mt-2 mt-md-0">
                 <Nav.Link href="#Aboutus">About Us</Nav.Link>
-                <Nav.Link href="#Aboutus">Business Sector</Nav.Link>
+                <Nav.Link href="#Bussec">Business Sector</Nav.Link>
                 <Nav.Link href="#Aboutus">Summary</Nav.Link>
                 <Nav.Link href="#Aboutus">Contact Us</Nav.Link>
               </Nav>
